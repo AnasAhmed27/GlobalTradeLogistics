d14235299647c49d8a084a6ac4a671066802d1c3
AnasAhmed27 <anasahmed6699@gmail.com>
2025-09-25 18:28:17 +0500

    First commit - global trade logistics

--- server/index.ts
+++ server/index.ts
@@ -60,10 +60,18 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-    const port = parseInt(process.env.PORT || "5000", 10);
+  //   const port = parseInt(process.env.PORT || "5000", 10);
 
-  server.listen(port, "127.0.0.1", () => {
-    log(`🚀 Server running on http://127.0.0.1:${port}`);
-  });
+  // server.listen(port, "127.0.0.1", () => {
+  //   log(`🚀 Server running on http://127.0.0.1:${port}`);
+  // });
+
+
+  const port = parseInt(process.env.PORT || "5000", 10);
+
+server.listen(port, "0.0.0.0", () => {
+  log(`🚀 Server running on http://0.0.0.0:${port}`);
+});
+
 
 })();
